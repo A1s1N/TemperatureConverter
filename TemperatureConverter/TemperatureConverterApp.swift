0e9ae1b8967aa32fb78979bc25abf31f3c8103dd
A1S1N <nikitosina71ru@gmail.com>
2022-09-05 14:04:42 +0300

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Никита Александров on 05.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
